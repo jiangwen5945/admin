b3c813fd2f618d7dad45f782cfeddcca4793f8b9
姜文 <32085768+jiangwen5945@users.noreply.github.com>
2023-05-03 02:39:10 +0800

    设置本地代理

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // 关闭eslint校验
-  chainWebpack: config =>{
+  chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
         args[0].title = "瞎猫管理系统";
         return args;
       })
+  },
+  // webpack-dev-server开启IP和域名访问权限
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: 'all'
   }
 })
